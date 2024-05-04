e67d554a07708b119c3ee928fd14cdbfa1bb15b2
Prathibha A S <prathibha230499@gmail.com>
2024-05-04 23:47:06 +0530

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+
 import NavBar from "./Components/NavBar.jsx";
 import Hero from "./Components/Hero.jsx";
+import About from "./Components/About.jsx";
 
 const App = () => {
   return(
     <div className="bg-black text-white">
       <NavBar />
       <Hero />
+      <About />
     </div>
   )
 }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
     return(
         <div className = " flex flex-col justify-center items-center text-white">
             <img src = {profile_img} alt="" className = "h-48 mt-[8%]"/>
-            <h1><span className = "bg-clip-text text-transparent bg-gradient-to-bl from-orange-400 to-purple-500 font-bold text-2xl">I'm Prathibha A S</span></h1>
+            <h1><span className = "bg-clip-text text-transparent bg-gradient-to-bl from-orange-600 to-purple-500 font-bold text-2xl">I'm Prathibha A S</span></h1>
             <p></p>
-            <div className = "">
-                <div>Resume</div>
-                <div>CV</div>
+            <div className = "flex">
+                <div className = "border border-white p-2 rounded-lg m-4 cursor-pointer hover:border-2 hover:scale-110 ease-in-out">Resume</div>
+                <div className = "border border-white p-2 rounded-lg m-4 cursor-pointer hover:border-2 hover:scale-110 ease-in-out">CV</div>
             </div>
         </div>
     )
